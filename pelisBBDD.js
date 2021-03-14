196a7d525afb8fb0ed5a712fa63dcbc54a552f7e
NataliSb9 <nsainzbarroso@gmail.com>
2021-03-14 12:41:12 +0100

    Peliculas API-MONGO

--- pelisBBDD.js
+++ pelisBBDD.js
@@ -2,6 +2,7 @@ let mongoose = require("mongoose");
 let Pelicula = require("./peliSchema.js");
 mongoose.connect('mongodb://localhost:27017/imdb', {useNewUrlParser: true, useUnifiedTopology: true})
 const express = require("express");
+const { findOneAndRemove } = require("./peliSchema.js");
 const app = express();
 app.use(express.json());
 
@@ -31,7 +32,7 @@ let peli1 = new Pelicula({
     platform: "Netflix",
 })
 
-//peli1.save(checkRespuesta)
+peli1.save(checkRespuesta)
 
 
 app.get("/peliculas", function (req, res) {
@@ -150,9 +151,129 @@ app.post("/peliculas",function(req,res){
 })
 
 app.post("/peliculas/actor",function(req,res){
-    if(req.body._id){}
+  let id = req.body._id;
+  Pelicula.findByIdAndUpdate({_id: id},
+    {$push:{actors: req.body.actors}}, function (err,pelicula){
+      if(err){
+        res.send(err)
+      }else{
+        res.send(`Se ha introducido un nuevo actor/actriz en la pelicula ${pelicula.title} correctamente`)
+      }
+    })
+})
+
+app.post("/peliculas/director",function(req,res){
+  let id = req.body._id;
+  Pelicula.findByIdAndUpdate({_id: id},
+    {$push:{director: req.body.director}}, function (err,pelicula){
+      if(err){
+        res.send(err)
+      }else{
+        res.send(`Se ha introducido un nuevo director/a en la pelicula ${pelicula.title} correctamente`)
+      }
+    })
+})
+
+app.post("/peliculas/guionista",function(req,res){
+  let id = req.body._id;
+  Pelicula.findByIdAndUpdate({_id: id},
+    {$push:{writer: req.body.writer}}, function (err,pelicula){
+      if(err){
+        res.send(err)
+      }else{
+        res.send(`Se ha introducido en la pelicula ${pelicula.title} un nuevo guionista correctamente`)
+      }
+    })
+})
+
+// Cambiar un elemto del
+app.put("/peliculas", function(req,res){
+  let id = req.body._id;
+  Pelicula.updateOne(
+    {_id: id},
+    {
+      title: req.body.title,
+      releaseYear: req.body.releaseYear,
+      actors: req.body.actors,
+      director: req.body.director,
+      writer: req.body.writer,
+      productora: req.body.productora,
+      distributor: req.body.distributor,
+      nationality: req.body.nationality,
+      genre: req.body.genre,
+      language: req.body.language,
+      mainCharacterName: req.body.mainCharacterName,
+      isMcu: req.body.isMcu,
+      platform: req.body.platform,
+    },
+    { omitUndefined: true },
+    function (err, pelicula) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(pelicula);
+      }
+    }
+  )
+})
+
+app.delete("/peliculas",function(req,res){
+  if(req.body._id !== undefined){
+    Pelicula.findOneAndRemove({_id:req.body._id},checkRespuesta)
+    function checkRespuesta(err,pelicula){
+      if(err){
+        res.send(err)
+      }else{
+        res.send(`La pelicula ${pelicula.title} ha sido eliminada correctamente de la base de datos`)
+      }
+    }
+  }
+})
+
+app.delete("/peliculas/actor",function(req,res){
+  let id= req.body._id
+  if(req.body._id !== undefined){
+    Pelicula.findOneAndUpdate({_id:req.body._id}, {$pull:{actors: req.body.actors}},checkRespuesta)
+    function checkRespuesta(err,pelicula){
+      if(err){
+        res.send(err)
+      }else{
+        res.send(`El profesional indicado ha sido borrado de la pelicula ${pelicula.title} correctamente`)
+      }
+    }
+  }
+})
+
+app.delete("/peliculas/director",function(req,res){
+  let id= req.body._id
+  if(req.body._id !== undefined){
+    Pelicula.findOneAndUpdate({_id:req.body._id}, {$pull:{director: req.body.director}},checkRespuesta)
+    function checkRespuesta(err,pelicula){
+      if(err){
+        res.send(err)
+      }else{
+        res.send(`El profesional indicado ha sido borrado de la pelicula ${pelicula.title} correctamente`)
+      }
+    }
+  }
+})
+
+
+app.delete("/peliculas/guionista",function(req,res){
+  if(req.body._id !== undefined){
+    Pelicula.findOneAndUpdate({_id:req.body._id}, {$pull:{writer: req.body.writer}},checkRespuesta)
+    function checkRespuesta(err,pelicula){
+      if(err){
+        res.send(err)
+      }else{
+        res.send(`El profesional indicado ha sido borrado de la pelicula ${pelicula.title} correctamente`)
+      }
+    }
+  }
 })
 app.listen(3000)
+
+
 
 
 
